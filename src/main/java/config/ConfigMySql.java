55fba077ac6a1e31e334d1722bb7c48c821f5020
Alejandro Gracia Ferrer <alejandrograferrerfpsuperior@gmail.com>
2026-04-29 14:15:27 +0200

    1 actualizacion

---
+++ src/main/java/config/ConfigMySql.java
@@ -0,0 +1,56 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import excepciones.BDException;
+
+public class ConfigMySql {
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URLBD = "jdbc:mysql://bdigmpecyv4wyyc16v8n-mysql.services.clever-cloud.com:3306/bdigmpecyv4wyyc16v8n";
+
+    private static final String usuario = "root";
+    private static final String contraseña = "";
+
+    /**
+     * Abre conexi�n con la base de datos mysql
+     * @return
+     * @throws BDException
+     */
+    public static Connection abrirConexion() throws BDException {
+        Connection conexion = null;
+
+        try {
+            // Carga el driver
+            Class.forName(DRIVER);
+            // Abre conexi�n
+            conexion = DriverManager.getConnection(URLBD, usuario, contraseña);
+        }
+        catch (ClassNotFoundException e) {
+            throw new BDException(BDException.ERROR_CARGAR_DRIVER + e.getMessage());
+        } catch (SQLException e) {
+            throw new BDException(BDException.ERROR_ABRIR_CONEXION + e.getMessage());
+        }
+
+        return conexion;
+
+    }
+
+    /**
+     * Cierra conexi�n con la base de datos
+     * @param conexion
+     * @throws BDException
+     */
+    public static void cerrarConexion(Connection conexion) throws BDException {
+
+        try {
+            conexion.close();
+        } catch (SQLException e) {
+            throw new BDException(BDException.ERROR_CERRAR_CONEXION + e.getMessage() );
+        }
+
+    }
+
+}
